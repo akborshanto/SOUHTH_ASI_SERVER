c5d855c468b1652a5c07f67696a3df137a5cce68
akbor shanto <akborshanto2002@gmail.com>
2024-04-30 14:37:48 -0700

    vercel deploy

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ require('dotenv').config()
 
 /* ====================================================================== */
 
+// const uri =
+//   `mongodb+srv://SOUTHEASTASIA:elkIin98K53Weqff@cluster1.phei2xm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 const uri =
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.phei2xm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
 
@@ -33,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  //  await client.connect();
 
     const spotCollection = client.db("addSpotDB").collection("addSpot");
     const internationlCollections = client.db("addSpotDB").collection("internationalSpot");
@@ -144,7 +146,7 @@ app.delete("/delete/:id",async (req,res)=>{
     /* ==============v✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅============ */
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
