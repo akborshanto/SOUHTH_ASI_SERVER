8085ddbadf251386b9f0b17c3ece17c758b75a23
akbor shanto <akborshanto2002@gmail.com>
2024-04-30 23:54:36 -0700

    text server site

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/allSpot',async(req,res)=>{
 
 /* view detail================ */
 app.get("/viewDetail/:id",async (req,res)=>{
-
+console.log(req.params.id)
     const result=await spotCollection.findOne({_id: new ObjectId(req.params.id)})
     console.log(result)
     res.send(result)
